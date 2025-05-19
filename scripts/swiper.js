9a766c34c7c94e7b0731ecb6dbde3b084b820447
Rostyslav Hubariev <r.gubarev1303@gmail.com>
2025-05-19 13:51:18 +0300

    initial commit

---
+++ scripts/swiper.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 'auto',
+    spaceBetween: 0,
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+  
